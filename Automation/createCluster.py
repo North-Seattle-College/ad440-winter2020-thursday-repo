9d4c9323f3b344435ebceebbc2dfaf7030c21cab
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-01-24 14:51:45 -0800

    Added checking of branch parameter, and autoconfigured dbClusterName based off of branch name

--- Automation/createCluster.py
+++ Automation/createCluster.py
@@ -2,27 +2,50 @@
 
 import boto3
 import sys
+import re
 
 rds = boto3.client('rds')
 
 username = sys.argv[1]
 password = sys.argv[2]
 securityGroupId = sys.argv[3]
-clusterIdentifier = sys.argv[4]
+github_branch = sys.argv[4]
+dbClusterIdentifier = ''
 
-try:
-  response = rds.create_db_cluster(
-    DBClusterIdentifier=clusterIdentifier,
-    MasterUsername=username,
-    MasterUserPassword=password,
-    VpcSecurityGroupIds=[
-        securityGroupId,
-    ],
-    Engine="aurora",
-    Port=3306
-  )
+# regex objects for compliant branch names
+development_exp = re.compile(r"development$")
+feature_exp = re.compile(r"feature-branch[1-6]-[a-zA-Z]{1,}$")
 
-  print(response)
+# check if branch name is correct format
+# branch needs to either be development or feature-branchx-username
+if development_exp.match(github_branch) or feature_exp.match(github_branch):
 
-except Exception as error:
-  print(error)
+    # convert branch name into DBClusterIdentifier
+    branch_components = github_branch.split('-')
+
+    # determine if it's the development branch or a feature branch
+    if len(branch_components) == 1:
+        dbClusterIdentifier = "dev-db-cluster"
+    else:
+        dbClusterIdentifier = "feature-cluster-" + branch_components[1] + "-" + branch_components[2]
+
+    #print(dbClusterIdentifier)
+    try:
+      response = rds.create_db_cluster(
+        DBClusterIdentifier=dbClusterIdentifier,
+        MasterUsername=username,
+        MasterUserPassword=password,
+        VpcSecurityGroupIds=[
+            securityGroupId,
+        ],
+        Engine="aurora",
+        Port=3306
+      )
+
+      print(response)
+
+    except Exception as error:
+      print(error)
+
+else: # if the github_branch does not follow the naming convention, then don't execute the script
+    print("The branch name does not follow convention, No database will be created.")
