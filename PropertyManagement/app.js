20d12fe4d198276eba441aad4d7595fd9c81d3c6
Kevin Law <kevin@Thaumaturge-Pro.local>
2020-01-23 18:05:07 -0800

    basic express added

--- PropertyManagement/app.js
+++ PropertyManagement/app.js
@@ -1,5 +1,12 @@
-app.get('/property', (req, res) => {
-    res.send({
-        property: []
-    })
+var express = require('express');
+var app = express();
+
+app.get('', (req, res) => {
+    res.send(
+        'Hello Express!'
+    )
+})
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+    
 })
