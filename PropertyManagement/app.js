992d5db80d533b510428a4a1c95b9f8dfa78d3af
Kevin Law <kevin@Thaumaturge-Pro.local>
2020-01-23 17:59:00 -0800

    added express

--- PropertyManagement/app.js
+++ PropertyManagement/app.js
@@ -1,8 +1,5 @@
-console.log('Starting')
-
-setTimeout(() => {
-    console.log('2 second Timer')
-}, 2000)
-
-
-console.log('Stopping')
+app.get('/property', (req, res) => {
+    res.send({
+        property: []
+    })
+})
