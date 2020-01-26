abcd9e4590a899194b287ab143fdb8def9ad7fda
robezal <robezal@gmail.com>
2020-01-26 13:55:50 -0800

    removed DS_store

--- Automation/ads3creation.py
+++ Automation/ads3creation.py
@@ -2,10 +2,13 @@
 import boto3
 s3 = boto3.client('s3')
 
-webite_configuration = {
-    "ErrorDocument":{"Key":"error.html"},
-    "IndexDocument":{"Suffix": "index.html"},
-}
-
-#set config
-s3.put_bucket_website(Bucket="firstbucket", WebsiteConfiguration=webite_configuration)
+try:
+    response = s3.create_bucket(
+        Bucket="robel-firstbucket",
+        CreateBucketConfiguration={
+        	"LocationConstraint":"us-west-2"
+        	},
+    )
+    print(response)
+except Exception as error:
+    print(error)
