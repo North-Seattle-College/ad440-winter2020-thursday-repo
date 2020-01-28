d4440af52a4f61e99535618c3ddc384ae99ec7cd
ReyesHerrera <rhperla.y@gmail.com>
2020-01-28 14:54:33 -0800

    skeleton homepage

--- UI/keytrack/src/App.js
+++ UI/keytrack/src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Navbar from './Components/navbar';
+import Home from './Components/home';
+import Account from './Components/account';
+import Logout from './Components/logout';
+import SideMenu from "./Components/SideMenu";
+
+const items = [
+  { name: 'Ckeckout Key', label: 'Ckeckout Key' },
+  { name: 'Add Property', label: 'Add Property' },
+  { name: 'Add Key ', label: 'Add Key' },
+
+]
+
+
+class App extends Component {
+
+    render() {
+        return (
+          <div class="container">
+            <BrowserRouter>
+                <div className="App">
+                    <Navbar />
+                    <Route exact path="/" render={() => <Home title="Key Manager" />} />
+                    <Route path="/Account/" render={() => <Account title="Account" />} />
+                    <Route path="/Logout/" render={() => <Logout title="Logout" />} />
+                </div>
+            </BrowserRouter>
+
+            <div class="fixed">
+              <SideMenu items={items}/>
+            </div>
+            <div class="flex-item">
+              <form>
+                <input type="text" id="filter" placeholder="Search for..."/>
+              </form>
+            </div>
+            </div>
+        );
+  }
 }
 
 export default App;
+
+
+
+
+// const items = [
+//   { name: 'home', label: 'Home' },
+//   { name: 'Ckeckout Key', label: 'Ckeckout Key' },
+//   { name: 'Add Property', label: 'Add Property' },
+//   { name: 'Add Key ', label: 'Add Key' },
+//
+// ]
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h>Umano Key Management</h>
+//       </header>
+//       <div>
+//         <SideMenu items={items}/>
+//       </div>
+//       <div className="searchForm">
+//             <form>
+//                 <input type="text" id="filter" placeholder="Search for..."/>
+//             </form>
+//       </div>
+//     </div>
+//   );
+// }
