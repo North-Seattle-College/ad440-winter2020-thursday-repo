2565aa51e6d35b4ed32124ca10cd68994832e2b3
Kevin Law <kevin@Thaumaturge-Pro.local>
2020-01-30 15:42:27 -0800

    added code comments

--- API/PropertyManagement/app.js
+++ API/PropertyManagement/app.js
@@ -1,19 +1,32 @@
-
+//set variables at the beginning
 exports.handler = async (event) => {
-    let body = event.body;
+    const body = event.body;
     var responseCode = null;
     var responseBody = null;
 
-    
     console.log("request: " + JSON.stringify(body));
     
+    //checks to see any of the required data is missing
     if (!body.property_id || !body.property_name || !body.property_type || !body.property_address ||
     !body.property_city || !body.property_state || !body.property_zip || !body.property_country){
         responseCode = 400;
         responseBody = {
-            message: "Missing data"
+            message: "Missing required data"
         };
+    
+    //checks to ensure that the right data type is provided
+    }else if(typeof body.property_id != 'number' || typeof body.property_name != 'string'||
+    typeof body.property_type != 'number' || typeof body.property_address != 'string' ||
+    typeof body.property_city != 'string' || typeof body.property_state != 'string' ||
+    typeof body.property_zip != 'number' || typeof body.property_country != 'string')
+    {
+        responseCode = 400;
+        responseBody = {
+            message: "Invalid datatype provided"
+        }
     }
+    //after both checks pass implement it 
+    //COMING SOON with access to database
     else{
         responseCode = 200;
         responseBody = body;
