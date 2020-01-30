eb5c5d2f88437ae6e1cc557e03b5e8f5ab2e90b4
Kevin Law <kevin@Thaumaturge-Pro.local>
2020-01-30 14:55:57 -0800

    added node.js

--- API/PropertyManagement/app.js
+++ API/PropertyManagement/app.js
@@ -1,12 +1,29 @@
-var express = require('express');
-var app = express();
 
-app.get('', (req, res) => {
-    res.send(
-        'Hello Express!'
-    )
-})
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+exports.handler = async (event) => {
+    let body = event.body;
+    var responseCode = null;
+    var responseBody = null;
+
     
-})
+    console.log("request: " + JSON.stringify(body));
+    
+    if (!body.property_id || !body.property_name || !body.property_type || !body.property_address ||
+    !body.property_city || !body.property_state || !body.property_zip || !body.property_country){
+        responseCode = 400;
+        responseBody = {
+            message: "Missing data"
+        };
+    }
+    else{
+        responseCode = 200;
+        responseBody = body;
+    }
+    
+    let response = {
+        statusCode : responseCode,
+        body: JSON.stringify(responseBody)
+    };
+    console.log("response: " + JSON.stringify(response));
+    return response;
+};
+
