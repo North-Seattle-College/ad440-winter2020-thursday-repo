c77c1713b02257190d89aba4f33b501c353f8d6f
robezal <robezal@gmail.com>
2020-02-09 00:15:10 -0800

    fixed bugs from sprint 1

--- Automation/creatings3static.py
+++ Automation/creatings3static.py
@@ -2,9 +2,23 @@
 import boto3
 s3 = boto3.client('s3')
 
+# description text
+text = "Creates a s3 function with the provided name and role."
+
+# initiate the parser
+parser = argparse.ArgumentParser(description = text)
+parser.add_argument("filename", help="s3 filename")
+
+# read argument from the command line
+args = parser.parse_args()
+
+# name to assign to the s3 function
+bucketname = args.filename
+
+
 try:
 	response=s3.put_bucket_website(
-		Bucket="robel-firstbucket",
+		Bucket="bucket",
 		WebsiteConfiguration={
 			"ErrorDocument":{
 				"Key":"error.html"
