5ecb3a1805366ee8470af9ddde173bce2f321bd8
Kevin Law <kevin@Thaumaturge-Pro.local>
2020-02-13 13:54:22 -0800

    added returning params

--- API/property/POST/postPropertyFinal.js
+++ API/property/POST/postPropertyFinal.js
@@ -11,29 +11,29 @@ function mysql_connection(){
 }
 
 exports.handler = (event, context, callback) => {
-  var connection = mysql_connection();
-  var queryParams = [
-    event.body.property_name,
-    event.body.property_type_id,
-    event.body.property_address,
-    event.body.property_city,
-    event.body.property_state,
-    event.body.property_zip,
-    event.body.property_country
+    var connection = mysql_connection();
+    var queryParams = [
+        event.body.property_name,
+        event.body.property_type_id,
+        event.body.property_address,
+        event.body.property_city,
+        event.body.property_state,
+        event.body.property_zip,
+        event.body.property_country
     ];
 
-  var sql = "INSERT INTO property (property_name, property_type_id, property_address, property_city, property_state, property_zip, property_country) VALUES ( ?,?,?,?,?,?,?)";
+    var sql = "INSERT INTO property (property_name, property_type_id, property_address, property_city, property_state, property_zip, property_country) VALUES ( ?,?,?,?,?,?,?)";
 
-  connection.query(sql,queryParams, (err, result) => {
-    if (err) {
-      throw err;
+    connection.query(sql,queryParams, (err, result) => {
+        if (err) {
+            throw err;
 
-      }
-    else{
-      console.log("Property Added!" + result);
-      connection.end();
-      callback(null, "Property created");
-    }  
-  });
+        }
+        else{
+            console.log("Property Added!" + result);
+            connection.end();
+            callback(null, "Property created: " + queryParams);
+        }
+    });
 };
 
