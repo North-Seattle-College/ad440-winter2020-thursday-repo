99a82586dda3b2f9eb3ca8034ca74cc8a5d542cf
YunFSui <syatwork@outlook.com>
2020-02-15 12:29:23 -0800

    added ways to CORS all responses (currently disabled, waitting for list of response pattern from api team.

--- Automation/api_gateway/cors_enable_apiGateway.py
+++ Automation/api_gateway/cors_enable_apiGateway.py
@@ -3,30 +3,45 @@ import boto3
 import json
 import apiGatewayLogger as logger
 
-stg_var_name_lst = ['keybundle_GET',
-                    'keybundle_id_DELETE',
-                    'keybundle_id_GET',
-                    'keybundle_id_PUT',
-                    'keyholder_GET',
-                    'keyholder_POST',
-                    'keyholder_id_DELETE',
-                    'keyholder_id_GET',
-                    'keyholder_id_PUT',
-                    'property_GET',
-                    'property_POST',
-                    'property_id_DELETE',
-                    'property_id_GET',
-                    'property_id_PUT',
-                    'property_id_keybundle_GET',
-                    'property_id_keybundle_POST'
-                    ]
+def main():
+  # set logger
+  logger.setLogger('api_gateway_cors_enable.log')
+  # get boto3 client
+  client = boto3.client('apigateway')
+  
+  # get api key with default to prod
+  api_id = GetAPIId()
+  # get a dict of resources for the API {resource_name:resource_id,[methods]}
+  resources_dict = GetResources(client, api_id)
+ 
+  # set Response Headers
+  response_headers = ['X-Requested-With', 'Access-Control-Allow-Headers',
+                      'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods']
+  resp_headers_methods = []
+  resp_headers_integration = []
+  for header in response_headers:
+    resp_headers_methods.append('method.response.header.'+header)
+    resp_headers_integration.append('integration.response.header.'+header)
+  print('Method Headers:')
+  print(resp_headers_methods)
+  print('Integration Headers:')
+  print(resp_headers_integration)
+  XRW_val = "'*'"
+  ACAH_val = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with'"
+  ACAO_val = "'*'"
+  
+  headers_vals = (XRW_val, ACAH_val, ACAO_val)
 
-#TODO set logger
-logger.setLogger('api_gateway_cors_enable.log')
-
-#TODO get boto3 client
-client = boto3.client('apigateway')
-
+  method_respPara = {}
+  for header in resp_headers_methods:
+    method_respPara[header] = True
+  print('Method Parameters: ')
+  print(method_respPara)
+  
+  PutCORSResponds(client, api_id, resources_dict, method_respPara, resp_headers_methods, headers_vals)
+  
+  
+  
 def GetAPIId():
   api_id = input('Enter API Gateway ID (press enter for default): ')
   if api_id == '':
@@ -34,128 +49,128 @@ def GetAPIId():
   logger.inputTrace('API ID', api_id)
   return api_id
 
-#TODO get a dict of resources for the API {resource_name:resource_id,[methods]}
-api_id = GetAPIId()
+def GetResources(client, api_id):
+  response_get_resources = client.get_resources(
+    restApiId=api_id
+  )
+  resources = response_get_resources['items']
 
-response_get_resources = client.get_resources(
-  restApiId=api_id
-)
-resources = response_get_resources['items']
+  resources_dict = {}
 
-resources_dict = {}
+  for resource in resources:
+    #print(resource)
+    resource_id = resource['id']
+    resource_path = resource['path']
 
-for resource in resources:
-  #print(resource)
-  resource_id = resource['id']
-  resource_path = resource['path']
+    methods = []
+    if resource_path != '/': # avoid root path, which don't have methods
+      resource_methods = resource['resourceMethods']
+    for method in resource_methods:
+      methods.append(method)
 
-  methods = []
-  if resource_path != '/': # avoid root path, which don't have methods
-    resource_methods = resource['resourceMethods']
-  for method in resource_methods:
-    methods.append(method)
+    resource = resource_path.split('/')
+    length = len(resource)
+    if length == 3:
+      resource = resource[-1].replace('{', '')
+      resource = resource.replace('}', '')
+    elif length == 4:
+      resource = resource[-2].replace('{', '') + '_' + resource[-1]
+      resource = resource.replace('}', '')
+    elif length == 2 and resource[-1] != '':
+      resource = resource[-1]
 
-  resource = resource_path.split('/')
-  length = len(resource)
-  if length == 3:
-    resource = resource[-1].replace('{', '')
-    resource = resource.replace('}', '')
-  elif length == 4:
-    resource = resource[-2].replace('{', '') + '_' + resource[-1]
-    resource = resource.replace('}', '')
-  elif length == 2 and resource[-1] != '':
-    resource = resource[-1]
+    if type(resource) == str:
+      temp_tuple = (resource_id, methods)
+      resources_dict[resource] = temp_tuple
+  logger.generatedDebug('API Resource Dictionary', json.dumps(resources_dict))
+  return resources_dict
 
-  if type(resource) == str:
-    temp_tuple = (resource_id, methods)
-    resources_dict[resource] = temp_tuple
-logger.generatedDebug('API Resource Dictionary', json.dumps(resources_dict))
+def PutCORSResponds(client, api_id, resources_dict, method_respPara, resp_headers_methods, headers_vals):
+  for resource in resources_dict:
+    logger.runTrace('for resource', resource)
+    resource_id = resources_dict[resource][0]
+    logger.runTrace('resouce', resource)
 
-#TODO Response Headers
-response_headers = ['X-Requested-With', 'Access-Control-Allow-Headers',
-                    'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods']
-resp_headers_methods = []
-resp_headers_integration = []
-for header in response_headers:
-  resp_headers_methods.append('method.response.header.'+header)
-  resp_headers_integration.append('integration.response.header.'+header)
-print('Method Headers:')
-print(resp_headers_methods)
-print('Integration Headers:')
-print(resp_headers_integration)
-XRW_val = "'*'"
-ACAH_val = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with'"
-ACAO_val = "'*'"
-
-method_respPara = {}
-for header in resp_headers_methods:
-  method_respPara[header] = True
-print('Method Parameters: ')
-print(method_respPara)
-
-for resource in resources_dict:
-  resource_id = resources_dict[resource][0]
-  logger.runTrace('resouce', resource)
-
-  methods = resources_dict[resource][1]
-  methods_str = ','.join(methods)
-  methods_str = "'" + methods_str + "'"
-  logger.runTrace('with methods', methods_str)
-
-  integration_mapping = [XRW_val, ACAH_val, ACAO_val, methods_str]
-  print('integration mapping:')
-  print(integration_mapping)
-  integration_respPara = dict(zip(resp_headers_methods, integration_mapping))
-  print('Integration Parameters: ')
-  print(integration_respPara)
-
-  
-  for method in methods:
-
-    logger.runTrace('for method', method)
-
-    #TODO delete Method Response Headers
-    try:
-      response_delete_method = client.delete_method_response(
-        restApiId = api_id,
-        resourceId = resource_id,
-        httpMethod = method,
-        statusCode = '200',
-      )
-      print(response_delete_method)
-    except:
-      logger.runTrace('Method deletion failed', 'Method does not exist')
-
-    #TODO delete Integration Response Headers
-    try:
-      response_delete_integration = client.delete_integration_response(
-        restApiId = api_id,
-        resourceId = resource_id,
-        httpMethod = method,
-        statusCode = '200',
-      )
-      print(response_delete_integration)
-    except:
-      print('Integration deletion failed', 'Integration does not exist')
-
-
-    #TODO put Method Response Headers
-    response_put_method = client.put_method_response(
-      restApiId = api_id,
-      resourceId = resource_id,
-      httpMethod = method,
-      statusCode = '200',
-      responseParameters = method_respPara
-    )
-    print(response_put_method)
+    methods = resources_dict[resource][1]
+    methods_str = ','.join(methods)
+    methods_str = "'" + methods_str + "'"
+    logger.runTrace('with methods', methods_str)
     
-    #TODO put Integration Responses Header Mappings
-    response_put_integration = client.put_integration_response(
-      restApiId = api_id,
-      resourceId = resource_id,
-      httpMethod = method,
-      statusCode = '200',
-      responseParameters = integration_respPara
-    )
-    print(response_put_integration)
+    integration_mapping = []
+    for header_val in headers_vals:
+      integration_mapping.append(header_val)
+    integration_mapping.append(methods_str)
+    logger.generatedDebug('Integration Mapping', json.dumps(integration_mapping))
+    
+    integration_respPara = {}
+    integration_respPara = dict(zip(resp_headers_methods, integration_mapping))
+    logger.generatedDebug('Integration Parameters', json.dumps(integration_respPara))
 
+    
+    for method in methods:
+      logger.runTrace('for method', method)
+      
+      #TODO get status codes
+      response_get_method = client.get_method(
+        restApiId = api_id,
+          resourceId = resource_id,
+          httpMethod = method
+      )
+      logger.generatedDebug('Method Detail', json.dumps(response_get_method))
+      method_responses = response_get_method['methodResponses']
+      logger.runTrace('Method Responses', json.dumps(method_responses))
+      status_codes = list(method_responses.keys())
+      logger.runTrace('Status Codes', json.dumps(status_codes))
+      #! hard coded to 200 only as waiting for API team update integration response selection pattern
+      status_codes = ['200']
+      
+      for status_code in status_codes:
+        logger.runTrace('for status code', status_code)
+        
+        #! delete Method Response Headers if there is one
+        try:
+          response_delete_method = client.delete_method_response(
+            restApiId = api_id,
+            resourceId = resource_id,
+            httpMethod = method,
+            statusCode = status_code, 
+          )
+          logger.runTrace('Method deletion', json.dumps(response_delete_method))
+        except:
+          logger.runTrace('Method deletion failed', 'Method does not exist')
+
+        #! delete Integration Response Headers if there is one
+        try:
+          response_delete_integration = client.delete_integration_response(
+            restApiId = api_id,
+            resourceId = resource_id,
+            httpMethod = method,
+            statusCode = status_code,
+          )
+          logger.runTrace('Integration deletion', json.dumps(response_delete_integration))
+        except:
+          logger.runTrace('Integration deletion failed', 'Integration does not exist')
+
+
+        #* put Method Response Headers
+        response_put_method = client.put_method_response(
+          restApiId = api_id,
+          resourceId = resource_id,
+          httpMethod = method,
+          statusCode = status_code,
+          responseParameters = method_respPara
+        )
+        print(response_put_method)
+        
+        #* put Integration Responses Header Mappings
+        response_put_integration = client.put_integration_response(
+          restApiId = api_id,
+          resourceId = resource_id,
+          httpMethod = method,
+          statusCode = status_code,
+          responseParameters = integration_respPara
+        )
+        print(response_put_integration)
+
+if __name__ == "__main__":
+    main()
