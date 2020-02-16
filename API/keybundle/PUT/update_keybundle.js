4cf3d833cea1769ab5b83dc08fbfff94f16ac8a9
deya taupier <30326911+miscdats@users.noreply.github.com>
2020-02-16 13:09:44 -0800

    Feature sprint2 miscdats (#164)
    * Update PUT keybundle function: add RDS connection.
    * DB successfully updates data with PUT request to Lambda endpoint.
    
    * Removal of property_id, as discussed in meeting.
    
    * Connection pool fix, move to inside exports.

--- API/keybundle/PUT/update_keybundle.js
+++ API/keybundle/PUT/update_keybundle.js
@@ -2,14 +2,6 @@ console.log("PUT keybundle :: function starting...");
 
 // mysql import and connection
 const mysql = require('mysql');
-const cnx = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_temp_USERNAME,
-  password : process.env.RDS_temp_PASSWORD,
-  port     : process.env.RDS_PORT,
-  database : process.env.RDS_DATABASE
-});
-console.log('Connecting...');
 
 exports.handler = (event, context, callback) => {
     try {
@@ -24,6 +16,7 @@ exports.handler = (event, context, callback) => {
 
         if (event) {//.method === 'PUT') { TODO: change checks back from debug mode
             if (1 != 1) { //.body.keybundle_id)) {
+                console.debug('400 status returned: bad input.');
                 response = {
                     statusCode: 400,
                     message:"Bad input!"
@@ -38,6 +31,16 @@ exports.handler = (event, context, callback) => {
                 response.keybundle_status_id = parseInt(event.body.keybundle_status_id);
                 response.keybundle_checkout_date = toSqlDatetime(date);
                 response.keybundle_due_date = toSqlDatetime(date.addDays(14));
+                
+                // check for inputs match
+                if (event.params.keybundle_id != response.keybundle_id) {
+                    console.debug('400 status returned: bad input.');
+                    response = {
+                        statusCode: 400,
+                        message:"Bad input!"
+                    };
+                    return response;
+                };
 
                 // update keybundle data from RDS with query
                 // TODO: check-in function do GET first
@@ -50,17 +53,28 @@ exports.handler = (event, context, callback) => {
                 ];
                 let update_keybundle_query = "UPDATE keybundle SET keybundle_status_id=?,keyholder_id=?,keybundle_checkout_date=?,keybundle_due_date=? WHERE keybundle_id=?;";
                 console.log('\n\nDATA: ', update_keybundle_query, query_data);
+                
+                
+                const cnx = mysql.createConnection({
+                  host     : process.env.RDS_HOSTNAME,
+                  user     : process.env.RDS_temp_USERNAME,
+                  password : process.env.RDS_temp_PASSWORD,
+                  port     : process.env.RDS_PORT,
+                  database : process.env.RDS_DATABASE
+                });
+                console.log('Connecting...');
                 cnx.query(update_keybundle_query, query_data, (error, results, callback) => {
                     if (error) {
-                        console.log('QUERY ERROR: ', error)
+                        console.error('QUERY ERROR: ', error);
+                        console.trace();
                         cnx.destroy();
-                        throw error;
+                        // throw error;
                     } else {
                         // connected!
                         console.log('QUERY SUCCESS: ', results);
-                        cnx.end();
                         // callback(null, results);
-                        console.log('OK: CONNECTION ENDED')
+                        cnx.end();
+                        console.info('OK: CONNECTION ENDED')
                     }
                 });
                 response = {
@@ -69,6 +83,7 @@ exports.handler = (event, context, callback) => {
                 }
             }
         } else { 
+            console.error('500 returned');
             response = {
                 statusCode: 500,
                 message:"Bad server!"
@@ -77,7 +92,7 @@ exports.handler = (event, context, callback) => {
         console.log('FINAL RESPONSE: ', response);
         return response;
     } catch (e) {
-        console.log('\n\nEXCEPTION: \n', e, '\n');
+        console.warn('\n\nEXCEPTION: \n', e, '\n');
         let response = { ERROR_MSG:e.message };
         return response;
     }
