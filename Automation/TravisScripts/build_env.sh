3b7059ed335cbd8d3fdd334807eb3d2e8a5ef443
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-02-17 09:49:05 -0800

    Removed npm test from travis script

---
+++ Automation/TravisScripts/build_env.sh
@@ -0,0 +1,17 @@
+FILE="./.env"
+
+if [[ $TRAVIS_BRANCH == 'feature-sprint3-mattyplo-test' ]]; then
+cat <<EOM >$FILE
+REACT_APP_NOT_SECRET_CODE=FEATURE
+EOM
+    echo "=========== $TRAVIS_BRANCH ENV ==========="
+    cat $FILE
+elif [[ $TRAVIS_BRANCH == 'master' ]]; then
+cat <<EOM >$FILE
+REACT_APP_SOME_SERVICE=development
+EOM
+    echo "=========== $TRAVIS_BRANCH ENV ==========="
+    cat $FILE
+else
+    exit 0
+fi
