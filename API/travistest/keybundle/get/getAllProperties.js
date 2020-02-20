2f833fb277219c28acd51a9c88ab0f0299b0de2b
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-02-20 10:52:15 -0800

    Added test directory and devops to allowable branch names to deploy lambda

---
+++ API/travistest/keybundle/get/getAllProperties.js
@@ -0,0 +1,24 @@
+//Get all properties
+//package for connecting to database
+const mysql = require('serverless-mysql')({
+
+  //credentials
+  config: {
+    host     : process.env.RDS_HOSTNAME,
+    port     : process.env.RDS_PORT,
+    database : process.env.RDS_DATABASE,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD
+  }
+});
+
+exports.handler = async (event, context) => {
+
+  //MYSQL query
+  let results = await mysql.query('SELECT * FROM property')
+
+  await mysql.end();
+
+  //return results
+  return results;
+}
