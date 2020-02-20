cbac5f3a6c97a46f87788897ef349525087e53c4
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-02-19 16:59:08 -0800

    Added Travis Branch variable to replaceDashes.sh script.  Added replaceDashes script to travis.  used the path set in the script.

--- Automation/TravisScripts/replaceDashes.sh
+++ Automation/TravisScripts/replaceDashes.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-branch=feature-sprint3-deya-property_id-put
 # This extracts the end of branch from the third '-'
 # This saves the extraction to the path_raw variable.
-path_raw=$( echo $branch | cut -d'-' -f4- )
+path_raw=$( echo $TRAVIS_BRANCH | cut -d'-' -f4- )
 
 # replace '-' with '/' and '_' with '/'
 path_raw_no_dashes=$( tr '-' '/' <<<$path_raw )
-path=$( tr '_' '/' <<<$path_raw_no_dashes )
-echo $path
+path_to_lambda=$( tr '_' '/' <<<$path_raw_no_dashes )
