7dc1079976761002a24de1ff7c15e4dee199b302
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-02-19 17:34:54 -0800

    Changed path to lambdas to have capitol API

--- Automation/TravisScripts/replaceDashes.sh
+++ Automation/TravisScripts/replaceDashes.sh
@@ -2,8 +2,10 @@
 
 # This extracts the end of branch from the third '-'
 # This saves the extraction to the path_raw variable.
+#TRAVIS_BRANCH=feature-sprint3-mattyplo-lambdas
 path_raw=$( echo $TRAVIS_BRANCH | cut -d'-' -f4- )
 
 # replace '-' with '/' and '_' with '/'
 path_raw_no_dashes=$( tr '-' '/' <<<$path_raw )
-export PATH_TO_LAMBDA=/API/$( tr '_' '/' <<<$path_raw_no_dashes )
+PATH_TO_LAMBDA=/API/$( tr '_' '/' <<<$path_raw_no_dashes )
+echo $PATH_TO_LAMBDA
