fa79f092c004ea2043af3a3e6f4e2b76c6e6b748
deya <30326911+miscdats@users.noreply.github.com>
2020-02-27 11:00:55 -0800

    #142: Get property by ID.

--- API/property/GET/GetPropertyByID/GET_property_by_id.js
+++ API/property/GET/GetPropertyByID/GET_property_by_id.js
@@ -1,89 +1,74 @@
-console.log("GET property by ID :: function starting...");
+console.info("GET property by ID :: function starting...");
 
-// mysql import and connection
-const mysql = require('mysql');
+// serverless mysql import and connection
+const cnx = require('serverless-mysql')({
+    config: {
+        host     : process.env.RDS_HOSTNAME,
+        user     : process.env.RDS_USERNAME,
+        password : process.env.RDS_PASSWORD,
+        port     : process.env.RDS_PORT,
+        database : process.env.RDS_DATABASE
+    }
+});
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context) => {
     try {
         let response = {
-            property_id: null,
-            property_name: null,
-            property_type_id: null,
-            property_address: null,
-            property_city: null,
-            property_state: null,
-            property_zip: null,
-            property_country: null
+            property_id: null
         };
 
+        // check methods and params or get error status code returned
         if (event.method != 'GET') {
-            console.debug('400 status returned: bad input.');
+            console.error('500 status returned: incorrect method call.');
             response = {
-                statusCode: 400,
-                message:"Bad input!"
-        };
-        // allows for using callbacks as finish/error-handlers
-        context.callbackWaitsForEmptyEventLoop = false;
-        
-        // grab param data to update
-        response.property_id = parseInt(event.pathParams.property_id);
-        
-        // check for inputs match
-        if (response.property_id < 0 || isNaN(response.property_id)) {
-            console.debug('400 status returned: bad input.');
-            response = {
-                statusCode: 400,
-                message:"Bad input!"
+                statusCode: 500,
+                message:"Server Error !"
             };
             return response;
-        };
-
-        // create query from data passed in request
-        let query_data = [
-            response.property_id,
-        ];
-        let get_property_query = "SELECT ? FROM property;";
-        console.log('\n\nDATA: ', get_property_query, query_data);
-        
-        const cnx = mysql.createConnection({
-            host     : process.env.RDS_HOSTNAME,
-            user     : process.env.RDS_temp_USERNAME,
-            password : process.env.RDS_temp_PASSWORD,
-            port     : process.env.RDS_PORT,
-            database : process.env.RDS_DATABASE
-        });
-        console.log('Connecting...');
-            cnx.query(get_property_query, query_data, (error, results, callback) => {
-                if (error) {
-                    console.error('QUERY ERROR: ', error);
-                    console.trace();
-                    cnx.destroy();
-                    // throw error;
-                } else {
-                    // connected!
-                    console.log('QUERY SUCCESS: ', results);
-                    // callback(null, results);
-                    cnx.end();
-                    console.info('OK: CONNECTION ENDED')
-                }
-            });
+            
+        } else if (isNaN(parseInt(event.params.property_id)) || 
+                    parseInt(event.params.property_id) < 0) {
+            console.debug('400 status returned: bad input.');
             response = {
-                statusCode: 200,
-                body: query_data
+                statusCode: 400,
+                message:"Bad Request !"
+            };
+            return response;
+        
+        } else {            
+            // grab param data to fetch
+            response.property_id = parseInt(event.params.property_id);
+    
+            // create then run sql query from data passed in request
+            let query_data = [
+                response.property_id,
+            ];
+            let get_property_query = "SELECT * FROM property WHERE property_id=?;";
+            console.info('\n\nRequesting...: ', query_data);
+            response = await cnx.query(get_property_query, query_data);
+            await cnx.end();
+            
+            // verify results
+            if (response.length < 1) {
+                console.trace();
+                console.debug('Queried not found + got: ', response);
+                response = {
+                    statusCode: 404,
+                    message:"Not Found !"
+                };
+                return response;
+            } else { // return requested info from successful query
+                console.log('FINAL RESPONSE: ', response);
+                return response[0];
             }
         }
-        // } else { 
-        //     console.error('500 returned');
-        //     response = {
-        //         statusCode: 500,
-        //         message:"Bad server!"
-        //     };
-        // };
-        console.log('FINAL RESPONSE: ', response);
-        return response;
+        
     } catch (e) {
         console.warn('\n\nEXCEPTION: \n', e, '\n');
-        let response = { ERROR_MSG:e.message };
+        console.error(e.message);
+        let response = { 
+            statusCode: 500, 
+            message: 'Server Error !' };
         return response;
-    };
+    }
 };
