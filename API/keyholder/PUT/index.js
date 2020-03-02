11af9109c9c80aee78f262cea8caedb2419e613b
Anu Slorah <anuslorah@hotmail.com>
2020-03-01 21:18:36 -0800

    adds minimal error handling

--- API/keyholder/PUT/index.js
+++ API/keyholder/PUT/index.js
@@ -1,6 +1,4 @@
 //PUT Keyholder by id 
-
-
 console.trace("PUT keyholder by keyholder_id -- function starting --");
  
 //node package for mysql connection
@@ -38,28 +36,38 @@ exports.handler = (event, context, callback) => {
     let email=event.body.email;
     let phone=event.body.phone;
     let keyholder_type_id=event.body.keyholder_type_id;  
-
-    console.debug("update input data: " + first_name, last_name, email, phone, keyholder_type_id);
-
-    //mysql update statement
-    let sql = "UPDATE keyholder SET first_name =? , last_name =? , email =? , phone =? , keyholder_type_id =? WHERE keyholder_id = " + keyholder_id ;
     
-    //mysql data to be filled inplace of the ?
-    let data = [first_name, last_name, email, phone, keyholder_type_id];
+    let error = new Error("wrong datatype inside json");
+    if(typeof first_name != 'string' || 
+     typeof last_name !='string'   || 
+     typeof email != 'string'     ||
+     typeof phone != 'string'     ||
+     typeof keyholder_type_id != 'number'){
+     
+        context.fail(error);
+    }else{
+        console.debug("update input data: " + first_name, last_name, email, phone, keyholder_type_id);
     
-    connection.query(sql,data, (err, result) => {
-        console.trace("mysql query initiated");
-        if (err) {
-            console.error("error", err);
-            throw err;
-
-        }
-        else{
-            console.info("keyholder updated" + result.message);
-            connection.end();
-            callback(null, result.message);
-        }
-    });
+        //mysql update statement
+        let sql = "UPDATE keyholder SET first_name =? , last_name =? , email =? , phone =? , keyholder_type_id =? WHERE keyholder_id = " + keyholder_id ;
+        
+        //mysql data to be filled inplace of the ?
+        let data = [first_name, last_name, email, phone, keyholder_type_id];
+        
+        connection.query(sql,data, (err, result) => {
+            console.trace("mysql query initiated");
+            if (err) {
+                console.error("error", err);
+                throw err;
+    
+            }
+            else{
+                console.info("keyholder updated" + result.message);
+                connection.end();
+                callback(null, result.message);
+            }
+        });
+    }
     console.trace("PUT keyholder by keyholder_id -- function end ");
 
 };
