882dc6d6fab2262958e0a5135491ee946865ddc7
kyleo83 <kyleostling@gmail.com>
2020-03-12 10:43:06 -0700

    input password now required

--- Automation/create_cognito_user.py
+++ Automation/create_cognito_user.py
@@ -5,6 +5,7 @@ import set_user_attributes
 import list_user_groups
 
 
+# Method to add users to groups
 def add_to_group(username):
     group_dict = list_user_groups.group_dict
     for group in group_dict:
@@ -26,30 +27,37 @@ parser = argparse.ArgumentParser(description = text)
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-c", "--create", help="create admin, employee & keyholder",
                     action="store_true")
-group.add_argument("-u", "--user", help="add username entered")
+group.add_argument("-u", "--user", help="create username entered")
 parser.add_argument("-d", "--delete", help="delete username entered")
 
-parser.add_argument("-a", "--add", help="add roles", action="store_true")
+parser.add_argument("-a", "--add", help="add users to groups", action="store_true")
 
 
 # read argument from the command line
 args = parser.parse_args()
 
 user_pool_id = 'us-west-2_xV6TGf7xl'
-password='AD440@testaccount'
+password=''
 
 # create cognito client
 client = boto3.client('cognito-idp')
 
 if args.create or args.user:
     if args.create:
-        user_dict = {'admin2':'9VZd@fakemail.com',
-                     'employee2':'2@fakemail.com',
-                     'keyholder2':'3@fakemail.com'}
+        # Bulk create all users
+        user_dict = {'admin':'9VZd@fakemail.com',
+                     'employee':'lUrq@fakemail.com',
+                     'keyholder':'N2qb@fakemail.com'}
     else:
+        # For individual user creation in lowercase
+        one_user = args.user.lower()
         email = input("Enter email address: ")
-        user_dict = {args.user:email}
+        user_dict = {one_user:email}
     try:
+        # Password will replace temp password set in next method
+        password = input("Enter permanent password: ")
+        
+        # Creates new user, email address, and temp password
         for username in user_dict:
             response = client.admin_create_user(
                 UserPoolId=user_pool_id,
@@ -60,22 +68,25 @@ if args.create or args.user:
                         'Value': user_dict[username],
                     },
                 ],
-#                TemporaryPassword='string',
                 ForceAliasCreation=False,
-#                MessageAction='RESEND',
                 DesiredDeliveryMediums=[
                     'EMAIL'
                 ],
             )
+            
+            # Calls methods to verify email and replace temp password
             set_user_attributes.set_password(user_pool_id, username, password)
             set_user_attributes.verify_email(user_pool_id, username)
+            
+            # If add option selected, add users to groups
             if args.add:
                 add_to_group(username)
 
 
     except Exception as error:
         print(error)
-        
+
+# Deletes user listed on the command line        
 elif args.delete:    
     try: 
         response = client.admin_delete_user(
--- Automation/set_user_attributes.py
+++ Automation/set_user_attributes.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 import boto3
 
-#aws cognito-idp admin-set-user-password --user-pool-id 'us-west-2_xV6TGf7xl' --username 'kyleo' --password 'H12!artS' --permanent
-#
-#aws cognito-idp admin-update-user-attributes --user-pool-id 'us-west-2_xV6TGf7xl' --username 'kyleo' --user-attributes Name=email_verified,Value=true
-
-#user_pool_id = 'us-west-2_xV6TGf7xl'
-#username = 'employee'
-#password='AD440@testaccount'
 
 client = boto3.client('cognito-idp')
-    
+
+# Method to replace temp password    
 def set_password(user_pool_id, username, password):
     response = client.admin_set_user_password(
         UserPoolId=user_pool_id,
@@ -19,8 +13,8 @@ def set_password(user_pool_id, username, password):
         Permanent=True
     )
 
+# Method to set Verify Email to True
 def verify_email(user_pool_id, username):  
-    print('User Name2: ' + username)
     response = client.admin_update_user_attributes(
         UserPoolId=user_pool_id,
         Username=username,
