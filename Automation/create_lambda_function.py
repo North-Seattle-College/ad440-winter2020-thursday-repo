61fbd9406df98390e4742bd95b75e62d96031022
YunFSui <syatwork@outlook.com>
2020-03-13 21:58:19 -0700

    fix a problem with pattern

--- Automation/create_lambda_function.py
+++ Automation/create_lambda_function.py
@@ -19,8 +19,8 @@ args = parser.parse_args()
 function_name = args.filename
 
 # requires Lamba name to follow the correct pattern
-feature_exp = re.compile(r"feature-sprint[1-6]-api-[a-z_]{1,}-[a-z0-9]{1,}$")
-devProd_exp = re.compile(r"dev|prod-[a-z_]{1,}-[a-z0-9]{1,}$")
+feature_exp = re.compile(r"feature-sprint[1-6]-api-[a-z_]{1,}-[a-z0-9-]{1,}$")
+devProd_exp = re.compile(r"dev|prod-[a-z_]{1,}-[a-z0-9-]{1,}$")
 
 
 if feature_exp.match(function_name) or devProd_exp.match(function_name):
