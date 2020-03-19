9e557ca75568e1138cdbd2e2e94ccfcfcf6e1a0b
deya <30326911+miscdats@users.noreply.github.com>
2020-03-19 13:51:19 -0700

    Rename of update_keybundle to index for convention sake of devops.

--- API/keybundle/id/put/index.js
+++ API/keybundle/id/put/index.js
@@ -1,9 +1,17 @@
 console.log("PUT keybundle :: function starting...");
 
-// mysql import and connection
-const mysql = require('mysql');
+// serverless mysql import and connection
+const cnx = require('serverless-mysql')({
+    config: {
+        host     : process.env.RDS_HOSTNAME,
+        user     : process.env.RDS_USERNAME,
+        password : process.env.RDS_PASSWORD,
+        port     : process.env.RDS_PORT,
+        database : process.env.RDS_DATABASE
+    }
+});
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context) => {
     try {
         let response = {
             keyholder_id: null,
@@ -14,85 +22,69 @@ exports.handler = (event, context, callback) => {
         };
         const date = new Date(Date.now());
 
-        if (event) {//.method === 'PUT') { TODO: change checks back from debug mode
-            if (1 != 1) { //.body.keybundle_id)) {
-                console.debug('400 status returned: bad input.');
-                response = {
-                    statusCode: 400,
-                    message:"Bad input!"
-                };
-            } else {
-                // allows for using callbacks as finish/error-handlers
-                context.callbackWaitsForEmptyEventLoop = false;
-                
-                // grab param data to update
-                response.keybundle_id = parseInt(event.body.keybundle_id);
-                response.keyholder_id = parseInt(event.body.keyholder_id);
-                response.keybundle_status_id = parseInt(event.body.keybundle_status_id);
-                response.keybundle_checkout_date = toSqlDatetime(date);
-                response.keybundle_due_date = toSqlDatetime(date.addDays(14));
-                
-                // check for inputs match
-                if (event.params.keybundle_id != response.keybundle_id) {
-                    console.debug('400 status returned: bad input.');
-                    response = {
-                        statusCode: 400,
-                        message:"Bad input!"
-                    };
-                    return response;
-                };
-
-                // update keybundle data from RDS with query
-                // TODO: check-in function do GET first
-                let query_data = [
-                    response.keybundle_status_id,
-                    response.keyholder_id,
-                    response.keybundle_checkout_date,
-                    response.keybundle_due_date,
-                    response.keybundle_id,
-                ];
-                let update_keybundle_query = "UPDATE keybundle SET keybundle_status_id=?,keyholder_id=?,keybundle_checkout_date=?,keybundle_due_date=? WHERE keybundle_id=?;";
-                console.log('\n\nDATA: ', update_keybundle_query, query_data);
-                
-                
-                const cnx = mysql.createConnection({
-                  host     : process.env.RDS_HOSTNAME,
-                  user     : process.env.RDS_temp_USERNAME,
-                  password : process.env.RDS_temp_PASSWORD,
-                  port     : process.env.RDS_PORT,
-                  database : process.env.RDS_DATABASE
-                });
-                console.log('Connecting...');
-                cnx.query(update_keybundle_query, query_data, (error, results, callback) => {
-                    if (error) {
-                        console.error('QUERY ERROR: ', error);
-                        console.trace();
-                        cnx.destroy();
-                        // throw error;
-                    } else {
-                        // connected!
-                        console.log('QUERY SUCCESS: ', results);
-                        // callback(null, results);
-                        cnx.end();
-                        console.info('OK: CONNECTION ENDED')
-                    }
-                });
-                response = {
-                    statusCode: 200,
-                    body: query_data
-                }
-            }
-        } else { 
+        // check method & inputs in path or return error
+        if (! event.method === 'PUT') {
             console.error('500 returned');
             response = {
                 statusCode: 500,
-                message:"Bad server!"
+                message:"Server Error !"
             };
-        };
-        console.log('FINAL RESPONSE: ', response);
-        return response;
+        } else if (event.params.keybundle_id) { 
+            console.debug('400 status returned: bad input.');
+            response = {
+                statusCode: 400,
+                message:"Bad Request !"
+            };
+        } else {            
+            // grab body data to update
+            response.keybundle_id = parseInt(event.body.keybundle_id);
+            response.keyholder_id = parseInt(event.body.keyholder_id);
+            response.keybundle_status_id = parseInt(event.body.keybundle_status_id);
+            response.keybundle_checkout_date = toSqlDatetime(date);
+            response.keybundle_due_date = toSqlDatetime(date.addDays(14));
+            
+            // check for inputs match
+            if (event.params.keybundle_id != response.keybundle_id) {
+                console.debug('400 status returned: bad input.');
+                response = {
+                    statusCode: 400,
+                    message:"Bad Request !"
+                };
+                return response;
+            };
+
+            // update keybundle data from RDS with query
+            let query_data = [
+                response.keybundle_status_id,
+                response.keyholder_id,
+                response.keybundle_checkout_date,
+                response.keybundle_due_date,
+                response.keybundle_id,
+            ];
+            let update_keybundle_query = "UPDATE keybundle SET keybundle_status_id=?,keyholder_id=?,keybundle_checkout_date=?,keybundle_due_date=? WHERE keybundle_id=?;";
+            console.log('\n\nDATA: ', update_keybundle_query, query_data);
+            
+            // db connection and run query or get error
+            console.log('Connecting...');
+            response = await cnx.query(get_property_query, query_data);
+            await cnx.end();
+            
+            // verify results
+            if (response.length < 1) {
+                console.trace();
+                console.debug('Queried not found + got: ', response);
+                response = {
+                    statusCode: 404,
+                    message:"Not Found !"
+                };
+                return response;
+            } else { // return requested info from successful query
+                console.log('FINAL RESPONSE: ', response);
+                return response[0];
+            }
+        }
     } catch (e) {
-        console.warn('\n\nEXCEPTION: \n', e, '\n');
+        console.warn('\nServer Error !\nEXCEPTION: \n', e, '\n');
         let response = { ERROR_MSG:e.message };
         return response;
     }
