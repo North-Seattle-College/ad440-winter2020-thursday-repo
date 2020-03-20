b4037a2c2fda13b4e8ec0791b394ee0505be436b
kyleo83 <kyleostling@gmail.com>
2020-03-19 22:38:20 -0700

    remove group added

--- Automation/create_cognito_user.py
+++ Automation/create_cognito_user.py
@@ -5,35 +5,50 @@ import re
 import list_user_groups
 
 
-# Method to add users to groups
-def check_group_name(grp_name, username):
+# Method to add or remove users to/from groups
+def update_group_name(grp_name, username, add):
     group_dict = list_user_groups.group_dict
     if grp_name in group_dict:
-        response = client.admin_add_user_to_group(
-            UserPoolId=user_pool_id,
-            Username=username,
-            GroupName=grp_name
-        )
+        if add:
+            response = client.admin_add_user_to_group(
+                UserPoolId=user_pool_id,
+                Username=username,
+                GroupName=grp_name
+            )
+        else:
+            response = client.admin_remove_user_from_group(
+                UserPoolId=user_pool_id,
+                Username=curr_user,
+                GroupName=group_name
+            ) 
+            
     else:
         print("'{}' doesn't match a valid Group Name.".format(grp_name))
         print("'{}' was not added to a group.".format(username))
-        print("Valid Groups: ")
-        print('{:15}'.format('Group'), '{:>10}'.format('Precedence'))
-        for group in group_dict:
-            print('{:15}'.format(group), '{:>10}'.format(group_dict[group])) 
+        list_group_precedences(group_dict)
 
+def list_groups_precedences(grp_dict):
+        print("\nValid Groups:\n")
+        print('{:15}'.format('Group'), '{:>10}'.format('Precedence'))
+        print('__________________________')
+        for group in grp_dict:
+            print('{:15}'.format(group), '{:>10}'.format(group_dict[group])) 
+            
 # description text
-text = "Creats or deletes users via cognito and adds roles to user via cognito"
+text = "Creates or deletes users via Cognito and adds roles to user via Cognito"
 
 # initiate the parser
 parser = argparse.ArgumentParser(description = text)
 group = parser.add_mutually_exclusive_group()
+group2 = parser.add_mutually_exclusive_group()
 group.add_argument("-n", "--new", help="create username entered")
 group.add_argument("-u", "--user", help="username for group update")
-parser.add_argument("-g", "--group", help="group to add")
-parser.add_argument("-r", "--remove", help="group to remove")
-parser.add_argument("-e", "--email", help='user email for new user')
 group.add_argument("-d", "--delete", help="delete username entered")
+group2.add_argument("-g", "--group", help="group to add")
+group2.add_argument("-r", "--remove", help="group to remove")
+parser.add_argument("-e", "--email", help='user email for new user')
+parser.add_argument("-l", "--list", action="store_true", 
+                    help="displays list of groups with precedences")
 
 # read argument from the command line
 args = parser.parse_args()
@@ -43,12 +58,12 @@ password=''
 
 email_exp = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
 
-# create cognito client
+# create Cognito client
 client = boto3.client('cognito-idp')
 email = ''
 
 if args.new:
-    # For individual user creation in lowercase
+    # For individual user creation in lower case
     one_user = args.new.lower()
     if args.email and email_exp.match(args.email):
         email = args.email
@@ -77,7 +92,7 @@ if args.new:
         # If add option selected, add users to groups
         if args.group:
             group_name = args.group
-            check_group_name(group_name, one_user)
+            update_group_name(group_name, one_user, True)
             
     except Exception as error:
         print(error)
@@ -87,14 +102,10 @@ elif args.user:
     try:
         if args.group:
                 group_name = args.group
-                check_group_name(group_name, curr_user)           
+                update_group_name(group_name, curr_user, True)           
         elif args.remove:     
             group_name = args.remove
-            response = client.admin_remove_user_from_group(
-                UserPoolId=user_pool_id,
-                Username=curr_user,
-                GroupName=group_name
-            )      
+            update_group_name(group_name, one_user, False)
         else:
             print("No action taken, add -g <groupname> or -r <groupname> at command line.")
             
@@ -125,3 +136,7 @@ elif args.delete:
 
 else:
     print("No action taken")
+    
+if args.list:
+    group_dict = list_user_groups.group_dict
+    list_groups_precedences(group_dict)
