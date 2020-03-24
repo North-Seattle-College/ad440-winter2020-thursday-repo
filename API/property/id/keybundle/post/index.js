0b35b036f49732305dd5e664fe8d7a1d5fc32acc
towa-hi <yamyamasunday@gmail.com>
2020-03-23 20:56:37 -0700

    added post keybundle by property id

--- API/property/id/keybundle/post/index.js
+++ API/property/id/keybundle/post/index.js
@@ -1,42 +1,66 @@
-//npm package for connecting to serverless database
-const mysql = require('mysql');
+const mysql = require('serverless-mysql')({
+  config: {
+    host     : process.env.RDS_HOSTNAME,
+    port     : process.env.RDS_PORT,
+    database : process.env.RDS_DATABASE,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD
+  }
+});
 
-    //credentials and connect to database
-    function mysql_connection(){
-        var params={
-        host     : process.env.RDS_HOSTNAME,
-        user     : process.env.RDS_USERNAME,
-        password : process.env.RDS_PASSWORD,
-        port     : process.env.RDS_PORT,
-        database : process.env.RDS_DATABASE
-    };
-    return mysql.createConnection(params);
-}
-
-exports.handler = async (event, context, callback) => {
-    
-    //connect to the database
-    var connection = mysql_connection();
-  
-    //get post data from the event body
-    var queryPost = [
-        event.body.keybundle_id,
-        event.body.keybundle_status, 
-        event.body.property_id,
-        event.body.keyholder_id.t
+exports.handler = async (event, context) => {
+  console.trace("POST keybundle by property id -- function starting --");
+  if (validate(event)) {
+    var queryParams = [
+    event.body.keybundle_id,
+    event.body.keybundle_status_id,
+    event.body.property_id,
+    event.body.keyholder_id,
+    event.body.keybundle_checkout_date,
+    event.body.keybundle_due_date
     ];
     
-    // sql query to insert keybundle object into database
-    var sql_post_keybundle = 'INSERT INTO keybundle (keybundle_id, keybundle_status_id, property_id, keyholder_id) VALUES (?, ?, ?, ?)';
-    
-    connection.query(connection, queryPost, (err, result)=>{
-        if (err){
-            throw err;
-        } else {
-            console.log('Keybundle added' + result);
-            connection.end();
-            callback(null, 'Property created: ' + queryPost);
-        }
-    });
-
+    var make_keybundle_query = 'INSERT INTO keybundle (keybundle_id, keybundle_status_id, property_id, keyholder_id, keybundle_checkout_date, keybundle_due_date) VALUES (?, ?, ?, ?, ?, ?);';
+    console.debug("Doing SQL: " + make_keybundle_query);
+    await mysql.query(make_keybundle_query, queryParams);
+    var get_inserted_row_query = 'SELECT * FROM keybundle WHERE keybundle_id=' + event.body.keybundle_id + ';';
+    console.debug("Doing SQL: " + get_inserted_row_query);
+    var newKeybundleList = await mysql.query(get_inserted_row_query);
+    let response = {
+      statusCode: 201,
+      body: newKeybundleList[0],
+      message: 'Created !'
+    };
+    console.trace('Returned new keybundle');
+    return context.succeed(response, response.headers);
+  } else {
+    let response = { 
+            statusCode: 400, 
+            errorMessage: 'Bad Request !' 
+    };
+    console.trace('Returned 400 bad request');
+    return context.fail(response.errorMessage, response.headers);
+  }
 };
+
+function validate(event) {
+  if (!isNumeric(event.body.keybundle_id)) {
+    return false;
+  }
+  if (!isNumeric(event.params.property_id)) {
+    return false;
+  }
+  if (!isNumeric(event.body.keybundle_status_id)) {
+    return false;
+  }
+  if (!isNumeric(event.body.property_id)) {
+    return false;
+  }
+  if (!isNumeric(event.body.keyholder_id)) {
+    return false;
+  }
+  return true;
+}
+function isNumeric(value) {
+        return /^\d+$/.test(value);
+}
