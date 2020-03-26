3f3b321985de41541928c7051a036926058d05aa
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-03-26 17:26:09 -0600

    Added exclamation point! to stuff.

--- API/property/id/get/index.js
+++ API/property/id/get/index.js
@@ -25,19 +25,19 @@ exports.handler = async (event, context) => {
                 errorMessage:"Server Error !"
             };
             return context.fail(response.errorMessage);
-            
-        } else if (isNaN(parseInt(event.params.property_id)) || 
+
+        } else if (isNaN(parseInt(event.params.property_id)) ||
                     parseInt(event.params.property_id) < 0) {
             console.debug('400 status returned: bad input.');
-            response = { 
+            response = {
                 statusCode: 400,
                 errorMessage: "Bad Request !" };
             return context.fail(response.errorMessage);
-        
-        } else {            
+
+        } else {
             // grab param data to fetch
             response.property_id = parseInt(event.params.property_id);
-    
+
             // create then run sql query from data passed in request
             let query_data = [
                 response.property_id,
@@ -46,14 +46,14 @@ exports.handler = async (event, context) => {
             console.info('\n\nRequesting...: ', query_data);
             response = await cnx.query(get_property_query, query_data);
             await cnx.end();
-            
+
             // verify results
             if (response.length < 1) {
                 console.trace();
                 console.debug('Queried not found + got: ', response);
                 response = {
                     statusCode: 404,
-                    errorMessage:"Not Found !"
+                    errorMessage:"Not Found !!"
                 };
                 return context.fail(response.errorMessage);
             } else { // return requested info from successful query
@@ -61,12 +61,12 @@ exports.handler = async (event, context) => {
                 return context.succeed(response[0]);
             }
         }
-        
+
     } catch (e) {
         console.warn('\n\nEXCEPTION: \n', e, '\n');
         console.error(e.message);
-        let response = { 
-            statusCode: 500, 
+        let response = {
+            statusCode: 500,
             errorMessage: 'Server Error !' };
         return context.fail(response.errorMessage);
     }
