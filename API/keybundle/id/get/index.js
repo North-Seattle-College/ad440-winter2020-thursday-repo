d7b035cdce23bae7cb5f003a8a5eb4d146c58c88
towa-hi <yamyamasunday@gmail.com>
2020-03-26 16:53:51 -0700

    keybundle_id get and delete

--- API/keybundle/id/get/index.js
+++ API/keybundle/id/get/index.js
@@ -5,13 +5,42 @@ const mysql = require('serverless-mysql')({
     database : process.env.RDS_DATABASE,
     user     : process.env.RDS_USERNAME,
     password : process.env.RDS_PASSWORD
-  }
+  },
+  onError: (e) => { console.log('MYSQL Error:',e.message) },
 });
 
 exports.handler = async (event, context) => {
-  let results = await mysql.query('SELECT * FROM keybundle')
+    console.trace("GET keybundle by id -- function starting --");
+    if (isNumeric(event.params.keybundle_id)) {
+        var get_keybundle_query = "SELECT * FROM keybundle WHERE keybundle_id=" + event.params.keybundle_id;
+        console.debug("Doing SQL: " + get_keybundle_query);
+        try {
+            var keybundleList = await mysql.query(get_keybundle_query)
+            console.debug("SQL server returned " + keybundleList);
+            if (keybundleList.length > 0) {
+                console.trace('Returned 200 Not Found: Failed to find keybundle id');
+                let response = {
+                    statusCode: 200,
+                    body: keybundleList[0],
+                    message: "Default "
+                }
+                return context.succeed(response);
+            } else {
+                console.trace('Returned 404 Not Found: Failed to find keybundle id');
+                return context.fail("Not Found ");
+            }
+            
+        } catch (error) {
+            console.trace('Returned 500 Server Error: Failed to get keybundle');
+            return context.fail("Server Error " + error);
+        }
+        
+    } else {
+        console.trace('Returned 400 Bad Request');
+        return context.fail("Bad Request !");
+    }
+};
 
-  await mysql.end();
-  // TODO pag
-  return results;
+function isNumeric(value) {
+        return /^\d+$/.test(value);
 }
