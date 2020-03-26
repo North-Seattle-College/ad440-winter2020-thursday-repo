9916fd8cd215c8915a95bda982586651f32b340e
Kevin Law <kevin@Thaumaturge-Pro.local>
2020-03-26 11:04:28 -0700

    Get All properties done with pagination

--- API/property/get/index.js
+++ API/property/get/index.js
@@ -1,57 +1,72 @@
-//Get all properties
-//package for connecting to database
-const mysql = require('serverless-mysql')({
+// Logging levels:
+//Trace: Everything.
+//Debug: What user entered, JSON request. What was implemented in the database. 
+//(information about database entry for post, put request, returned get request)
+//Info: What happened. POST request, PUT REQUEST, GET REQUEST, DELETE. Skeleton of what happened.
+//Error: error, 400, 404, 405, 500 would show up
 
-  //credentials
-  config: {
-    host     : process.env.RDS_HOSTNAME,
-    port     : process.env.RDS_PORT,
-    database : process.env.RDS_DATABASE,
-    user     : process.env.RDS_USERNAME,
-    password : process.env.RDS_PASSWORD
-  }
-});
 
-exports.handler = async (event, context) => {
-    try {
-        let response = {
-            
-        };
+
+//node package for mysql connection
+const mysql = require('mysql');
+
+//credentials needed to connect
+function mysql_connection(){
+    var params={
+        host     : process.env.RDS_HOSTNAME,
+        user     : process.env.RDS_USERNAME,
+        password : process.env.RDS_PASSWORD,
+        port     : process.env.RDS_PORT,
+        database : process.env.RDS_DATABASE
+    };
+    return mysql.createConnection(params);
+}
+
+console.trace("Get All Property-- function starting --");
+exports.handler = (event, context, callback) => {
+    
+    var connection = mysql_connection();
+    console.trace("Successfully connected to the database");
+
+    //Query to know number of rows in the property database
+    var query = "SELECT count(*) as numRows FROM property";
+    var numRows;
+
+    console.trace("connecting to the database to get the number of rows")
+    connection.query(query, (err, res) => {
+    if (err){
+        console.error("Database Error");
+        return context.fail("Server Error" + err);
+    }else{
+        numRows = res[0].numRows;
+        console.trace("Number of Rows: " + numRows)
         
-        if (event.method != "GET") {
-            console.error("500 status returned: incorrect method call");
-            response = {
-                statusCode: 500,
-                errorMessage:"Server Error"
-            };
-            return context.fail(response.errorMessage);
-        } else {
-            //MYSQL query
-            let results = "SELECT * FROM property JOIN propertytype ON property.property_type_id=propertytype.property_type_id;"
-            response = await mysql.query(results);
-            await mysql.end();
+        // set default value for record per page to number of rows in property table
+        // set default skip value to be zero
+        var numPerPage = parseInt(event.query.limit, 10) || numRows;
+        var skip = parseInt(event.query.skip, 10) || 0;
+        
+        var limit = skip + ',' + numPerPage;
+        console.debug("limit: ", limit);
+    }
+                     
+    //mysql select all statement
+    var select = "SELECT * FROM property JOIN propertytype ON property.property_type_id=propertytype.property_type_id LIMIT " + limit ;
+    connection.query(select, (err, result) => {
+    console.trace("Select All Property Query initiated");
 
-            
-            //verify results
-            if (response.length <1){
-                response = {
-                    statusCode: 404,
-                    errorMessage: "Not Found !"
-                };
-                return context.fail(response.errorMessage);
-                } else //return requested info from successful GET query 
-                {
-                    return response;
+        if (err) {
+            console.error("Database Error");
+            return context.fail("Server Error " + err);
+        }
+        else{
+            console.log("Property Added!" + result);
+            connection.end();
+            callback(null, result);
+        }
+    });
+     console.trace("GET Property -- function end ");
 
-                }
-                   
-        }
-    } catch (e) {
-        console.error(e.message);
-        let response = {
-            statusCode: 500,
-            errorMessage: "Server Error !" };
-        return context.fail(response.errorMessage);
-        }
+    
+});
 };
-  
